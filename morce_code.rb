eb9211481c5b5e86755f94871ec00405fd991aea
Regiss Mukubiza <regiss.steven@gmail.com>
2022-07-20 16:40:08 +0200

    add logic

--- morce_code.rb
+++ morce_code.rb
@@ -1,11 +1,65 @@
-def decode_morse(morse_code)
-    morse_code
-      .split('   ')
-      .select do |char|
-        char != ''
+MORSE_DICTIONARY = {
+    '.-' => 'a',
+    '-...' => 'b',
+    '-.-.' => 'c',
+    '-..' => 'd',
+    '.' => 'e',
+    '..-.' => 'f',
+    '--.' => 'g',
+    '....' => 'h',
+    '..' => 'i',
+    '.---' => 'j',
+    '-.-' => 'k',
+    '.-..' => 'l',
+    '--' => 'm',
+    '-.' => 'n',
+    '---' => 'o',
+    '.--.' => 'p',
+    '--.-' => 'q',
+    '.-.' => 'r',
+    '...' => 's',
+    '-' => 't',
+    '..-' => 'u',
+    '...-' => 'v',
+    '.--' => 'w',
+    '-..-' => 'x',
+    '-.--' => 'y',
+    '--..' => 'z',
+    ' ' => ' ',
+    '.----' => '1',
+    '..---' => '2',
+    '...--' => '3',
+    '....-' => '4',
+    '.....' => '5',
+    '-....' => '6',
+    '--...' => '7',
+    '---..' => '8',
+    '----.' => '9',
+    '-----' => '0'
+  }.freeze
+  
+  def convert_char(char)
+    return  MORSE_DICTIONARY[char].capitalize
+    
+    end
+    def convert_word(word)
+      if word.is_a? String
+        word_converted = ''
+        word.split.each do |morse|
+          dc = convert_char(morse)
+          word_converted += dc.to_s
+        end
       end
-      .map do |word|
-        word.split(' ').map { |char| MORSE_CODE[char] }.join
+      word_converted
+    end
+    def convert_sentence(sentence)
+      if sentence.is_a? String
+          sentence_converted=''
+          sentence.split('   ').each do |text|
+             dc= convert_word(text)
+             sentence_converted +="#{dc} "
+          end
       end
-      .join(' ')
+      sentence_converted.strip
   end
+    p convert_sentence('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
